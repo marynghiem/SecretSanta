ec4699e498854c7e9c5b847619f15167b9440d9f
marynghiem <marynghiem46@gmail.com>
2021-11-24 19:01:00 -0800

    adding email through input box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [emails, setEmails] = useState(['mary@gmail.com', 'joseph@gmail.com']);
+  const [currentEmail, setCurrentEmail] = useState('');
   // emails: ['mary@gmail.com', 'joseph@gmail.com'];
   // setEmails(['mary@gmail.com', 'joseph@gmail.com']);
   // const fakeEmailList2 = ['mary@gmail.com', 'joseph@xgmail.com'];
@@ -10,11 +11,15 @@ function App() {
   // No: emails + newEmail -> cant add arrays with + sign
   // No: emails.push(newEmail) -> push returns number & react expects A NEW ARRAY
 
-  const addEmail = () => setEmails([...emails, newEmail]);
+  const addEmail = () => setEmails([...emails, currentEmail]);
+  const onEmailInputChange = (event) => {
+    setCurrentEmail(event.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Secret Santa</h1>
-      <input type="email" placeholder="Email"></input>
+      <h1 id="titleName">Secret Santa</h1>
+      <input type="email" onChange={onEmailInputChange} value={currentEmail} placeholder="Email"></input>
       <button type="button" onClick={addEmail}>Add Email</button>
       <ul class="emailList">{emails.map((emailList)=> <li>{emailList}</li>)}</ul>
     </div>
