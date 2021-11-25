10ae6027c324b34b6685e96524db52cd66fb43cf
marynghiem <marynghiem46@gmail.com>
2021-11-24 18:45:11 -0800

    added a gradient background and added an input for email

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [emails, setEmails] = useState([]);
+  const [emails, setEmails] = useState(['mary@gmail.com', 'joseph@gmail.com']);
   // emails: ['mary@gmail.com', 'joseph@gmail.com'];
-  const fakeEmailList1 = 'jose@yahoo.com';
-  const fakeEmailList2 = ['mary@gmail.com', 'joseph@gmail.com'];
+  // setEmails(['mary@gmail.com', 'joseph@gmail.com']);
+  // const fakeEmailList2 = ['mary@gmail.com', 'joseph@xgmail.com'];
+  const newEmail = 'example@gmail.com';
+  // No: emails + newEmail -> cant add arrays with + sign
+  // No: emails.push(newEmail) -> push returns number & react expects A NEW ARRAY
+
+  const addEmail = () => setEmails([...emails, newEmail]);
   return (
     <div className="App">
+      <h1>Secret Santa</h1>
       <input type="email" placeholder="Email"></input>
-      <button type="button">Add Email</button>
-      <p>{fakeEmailList1}</p>
+      <button type="button" onClick={addEmail}>Add Email</button>
+      <ul class="emailList">{emails.map((emailList)=> <li>{emailList}</li>)}</ul>
     </div>
   );
 }
 
 export default App;
+
+var list = []; // 0x123345352
+list.push('hi'); // 1
+// list = ['hi']
