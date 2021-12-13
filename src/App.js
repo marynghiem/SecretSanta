203b142b1815262f6a40e3031252ec24b167bd3e
marynghiem <marynghiem46@gmail.com>
2021-12-13 12:41:54 -0800

    made a red container for the other elements

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [emails, setEmails] = useState(['mary@gmail.com', 'joseph@gmail.com']);
+  const [emails, setEmails] = useState([]);
   const [currentEmail, setCurrentEmail] = useState('');
-  // emails: ['mary@gmail.com', 'joseph@gmail.com'];
-  // setEmails(['mary@gmail.com', 'joseph@gmail.com']);
-  // const fakeEmailList2 = ['mary@gmail.com', 'joseph@xgmail.com'];
-  const newEmail = 'example@gmail.com';
-  // No: emails + newEmail -> cant add arrays with + sign
-  // No: emails.push(newEmail) -> push returns number & react expects A NEW ARRAY
+  
 
   const addEmail = () => setEmails([...emails, currentEmail]);
   const onEmailInputChange = (event) => {
@@ -18,16 +13,18 @@ function App() {
 
   return (
     <div className="App">
+      <div class="container">
       <h1 id="titleName">Secret Santa</h1>
-      <input type="email" onChange={onEmailInputChange} value={currentEmail} placeholder="Email"></input>
+      <p class="description">Secret Santa is a game where a group of individuals anonymously exchange gifts to one another.
+         To help with the process of drawing names, this generator will take a list of emails, shuffle them, and send your Secret Santa to your email. </p>
+      <input type="email"onChange={onEmailInputChange} value={currentEmail} placeholder="Email"></input>
       <button type="button" onClick={addEmail}>Add Email</button>
-      <ul class="emailList">{emails.map((emailList)=> <li>{emailList}</li>)}</ul>
+      <div class="unorderedListDiv">
+      <ul class="emailList">{emails.map((emailList)=> <li>{emailList}</li>)}</ul></div>
+      </div>
     </div>
   );
 }
 
 export default App;
 
-var list = []; // 0x123345352
-list.push('hi'); // 1
-// list = ['hi']
