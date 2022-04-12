b0d16deab484f1c282628092541b5fb440790cf7
marynghiem <marynghiem46@gmail.com>
2022-04-11 20:10:33 -0700

    Update env file to auto load sengrid api key

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 // server/index.js
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const { shuffleParticipants } = require("./SecretSantaShuffler.js");
